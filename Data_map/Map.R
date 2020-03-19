2eeeb67e0756623e25e314b183a9d5fcd1cc5d7c
manahil-b <manahil_b@hotmail.com>
2020-03-19 15:44:37 -0400

    assignment 2
    map

---
+++ Data_map/Map.R
@@ -0,0 +1,40 @@
+install.packages('cancensus')
+library(cancensus) 
+
+options(cancensus.api_key='CensusMapper_6233c02eaf983891865dbb05f67ce4fd')
+
+options(cancensus.cancensus.cache_path="~")
+#Just of the population in mississauga
+census_data <- get_census(dataset='CA16', regions=list(CSD="3521005"), vectors=c(), labels="detailed", geo_format="sf", level='DA')
+
+library(tidyverse) 
+glimpse(census_data)
+
+library(ggplot2)
+ggplot(census_data, aes(geometry=geometry))+geom_sf()
+ggplot(census_data, aes(geometry=geometry, fill=Population))+geom_sf()
+
+
+#looking at the different minority groups in mississauga
+install.packages("cancensus")
+library(cancensus)
+options(cancensus.api_key='CensusMapper_6233c02eaf983891865dbb05f67ce4fd')
+options(cancensus.cache_path = <your long term cache path>)
+census_data2 <- get_census(dataset='CA16', regions=list(CSD="3521005"), vectors=c("v_CA16_3960","v_CA16_3963","v_CA16_3966","v_CA16_3969","v_CA16_3972","v_CA16_3975","v_CA16_3978","v_CA16_3981","v_CA16_3984","v_CA16_3987","v_CA16_3996"), labels="detailed", geo_format="sf", level='DA')
+glimpse(census_data2)
+#rename variables
+mississauga<-rename(census_data2, "south asian"=`v_CA16_3960: South Asian`,"black"=`v_CA16_3966: Black`,"chinese"=`v_CA16_3963: Chinese`,"filipino"=`v_CA16_3969: Filipino`,"latin american"=`v_CA16_3972: Latin American`,"arab"=`v_CA16_3975: Arab`,"southeast asian"=`v_CA16_3978: Southeast Asian`,"west asian"=`v_CA16_3981: West Asian`,"korean"=`v_CA16_3984: Korean`,"japanese"=`v_CA16_3987: Japanese`,"not a visible minority"=`v_CA16_3996: Not a visible minority`)
+ names(mississauga)                   
+                    
+ggplot(mississauga, aes(geometry=geometry, fill=
+                          ))+geom_sf()      
+
+
+
+
+
+
+
+
+
+
